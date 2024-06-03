91bd4dc05d1e705717a28a88497085c8f35f3660
Zac Caddick <caddickzac@gmail.com>
2024-06-03 14:50:26 -0700

    updates

--- CognitiveTestsProcessors_StreamlitApp/CognitiveTestsProcessors.py
+++ CognitiveTestsProcessors_StreamlitApp/CognitiveTestsProcessors.py
@@ -6,19 +6,16 @@ def main():
     st.write("Python scripts for efficiently processing data from cognitive tests (PVT, DSST, and Serial Addition)")
 
     st.markdown("## Overview")
-    st.markdown("Each script within this suite is tailored to a specific cognitive test, facilitating both individual and batch processing of test data. The scripts support various output options, including combined and separated outputs, providing flexibility for data analysis and research purposes.")
+    st.markdown("Each script within this suite is tailored to a specific E-Prime cognitive test.")
 
     st.markdown("## Processing Scripts")
     st.markdown("**DSST**:")
-    st.markdown(" - `DSST_to_ZippedCSVs.py`: Packages multiple PVT test outputs into individual CSV files and zips them into a single file.")
     st.markdown(" - `DSST_to_CombinedCSV.py`: Combines multiple PVT test outputs into a single CSV file.")
 
     st.markdown("**PVT**:")
-    st.markdown(" - `PVT_to_ZippedCSVs.py`: Packages multiple PVT test outputs into individual CSV files and zips them into a single file.")
     st.markdown(" - `PVT_to_CombinedCSV.py`: Combines multiple PVT test outputs into a single CSV file.")
 
     st.markdown("**Serial Addition Test**:")
-    st.markdown(" - `serialAddition_to_ZippedCSVs.py`: Packages multiple PVT test outputs into individual CSV files and zips them into a single file.")
     st.markdown(" - `serialAddition_to_CombinedCSV.py`: Combines multiple PVT test outputs into a single CSV file.")
 
 if __name__ == "__main__":
--- CognitiveTestsProcessors_StreamlitApp/pages/PVT_to_CombinedCSV.py
+++ CognitiveTestsProcessors_StreamlitApp/pages/PVT_to_CombinedCSV.py
@@ -68,8 +68,13 @@ def main():
         file_counter = 0
 
         for uploaded_file in uploaded_files:
-            file_counter += 1  # Increment counter for each new file
             content = uploaded_file.getvalue().decode("utf-16").replace("\t", "").splitlines()
+
+            if not content:  # Check if the file is empty
+                st.warning(f"File {uploaded_file.name} is empty and will be skipped.")
+                continue  # Skip the rest of the loop for this file
+            
+            file_counter += 1  # Increment counter for each new file
 
             # Your processing code to extract metadata and log frame data
             meta_data = extract_metadata_from_content(content)
@@ -100,16 +105,19 @@ def main():
             # Append the DataFrame for this file to the list
             all_data_frames.append(final_df)
 
-        # Combine all individual file DataFrames into one
-        combined_df = pd.concat(all_data_frames, ignore_index=True)
+        if all_data_frames:  # Check if there's any data to combine
+            # Combine all individual file DataFrames into one
+            combined_df = pd.concat(all_data_frames, ignore_index=True)
 
-        # Show and provide download for the combined DataFrame
-        st.dataframe(combined_df)
-        combined_csv = combined_df.to_csv(index=False).encode('utf-8')
-        st.download_button(label="Download Combined CSV",
-                           data=combined_csv,
-                           file_name="combined_processed_data.csv",
-                           mime='text/csv')
+            # Show and provide download for the combined DataFrame
+            st.dataframe(combined_df)
+            combined_csv = combined_df.to_csv(index=False).encode('utf-8')
+            st.download_button(label="Download Combined CSV",
+                               data=combined_csv,
+                               file_name="combined_processed_data.csv",
+                               mime='text/csv')
+        else:
+            st.warning("No data to process. Please upload valid files.")
 
 if __name__ == "__main__":
     main()
